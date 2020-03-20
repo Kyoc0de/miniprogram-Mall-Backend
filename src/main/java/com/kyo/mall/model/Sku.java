5374f1f2a07e982f1af139bd3b5453c430d450f3
kyoo0 <kyoscode@gmail.com>
2020-03-20 16:32:40 -0400

    泛型序列化

--- src/main/java/com/kyo/mall/model/Sku.java
+++ src/main/java/com/kyo/mall/model/Sku.java
@@ -1,20 +1,21 @@
 package com.kyo.mall.model;
 
-import com.kyo.mall.util.ListAndJson;
-import com.kyo.mall.util.MapAndJson;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.kyo.mall.util.GenericAndJson;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
-import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
+@Entity
 @Getter
 @Setter
-@Entity
+//@TypeDef(name="json", typeClass = JsonStringType.class)
 public class Sku extends BaseEntity {
     @Id
     private Long id;
@@ -24,14 +25,25 @@ public class Sku extends BaseEntity {
     private String img;
     private String title;
     private Long spuId;
-
-    private String code;
-    private Long stock;
     private Long categoryId;
     private Long rootCategoryId;
 
-//    @Convert(converter = MapAndJson.class)
-//    private Map<String,Object> test;
-    @Convert(converter = ListAndJson.class)
-    private List<Object> specs;
+    private String  specs;
+    private String code;
+    private Long stock;
+
+    public List<Spec> getSpecs() {
+        if (this.specs == null) {
+            return Collections.emptyList();
+        }
+        return GenericAndJson.jsonToObject(this.specs, new TypeReference<List<Spec>>(){});
+    }
+
+    public void setSpecs(List<Spec> specs) {
+        if(specs.isEmpty()){
+            return;
+        }
+        this.specs = GenericAndJson.objectToJson(specs);
+    }
+
 }
